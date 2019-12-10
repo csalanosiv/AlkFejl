df0ec2d7ff5bc0c59b8c8986e535e9bdbbb66581
csalanosiv <55079503+csalanosiv@users.noreply.github.com>
2019-12-10 10:25:27 +0100

    Add files via upload

---
+++ Foxtrot/src/main/java/com/mycompany/foxtrot/entities/Work.java
@@ -0,0 +1,44 @@
+package com.mycompany.foxtrot.entities;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Work implements Serializable {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    
+    @Column(nullable = false)
+    private String name;
+    
+    @Column(nullable = false)
+    private String description;
+   
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Status status;
+    
+    public enum Status {
+        NEW, INPROGRESS, FINISHED
+    }
+}
+
